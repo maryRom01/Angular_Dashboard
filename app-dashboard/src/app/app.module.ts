92ffeb24a1a143c7ecb582c1f4a9ed248b8f3d4d
Maryna Romanets <mromanets@fingercheck.com>
2023-06-10 16:16:19 -0400

    add empty components (header, footer, navigation-panel

--- app-dashboard/src/app/app.module.ts
+++ app-dashboard/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { NavigationPanelComponent } from './core/navigation-panel/navigation-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationPanelComponent
   ],
   imports: [
     BrowserModule,
---
+++ app-dashboard/src/app/core/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dsh-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
