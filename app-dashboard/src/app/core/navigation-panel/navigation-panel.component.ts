398fd83d489844f1d727a7439061246c6b09fd58
Maryna Romanets <mromanets@fingercheck.com>
2023-06-12 09:18:35 -0400

    add sub links

--- app-dashboard/src/app/core/navigation-panel/navigation-panel.component.ts
+++ app-dashboard/src/app/core/navigation-panel/navigation-panel.component.ts
@@ -6,11 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-panel.component.scss']
 })
 export class NavigationPanelComponent {
-  navLinks: any[] = [
-    {name: 'Dashboard'},
-    {name: 'Components'},
-    {name: 'Tables'},
-    {name: 'Maps'},
-    {name: 'Menu Level 1'}
+  menuLevelSubLinks: any[] = [
+    {name: 'Menu Level 1.1'},
+    {name: 'Menu Level 1.2'},
+    {name: 'Menu Level 1.3'}
   ]
+  navLinks: any[] = [
+    {name: 'Dashboard', subMenu: false},
+    {name: 'Components', subMenu: false},
+    {name: 'Tables', subMenu: false},
+    {name: 'Maps', subMenu: false},
+    {name: 'Menu Level 1', subMenu: true, options: this.menuLevelSubLinks}
+  ];
+
 }
