74b60292412cd7d9a1797a546cf8051123a7d9d9
Maryna Romanets <mromanets@fingercheck.com>
2023-06-17 17:39:52 -0400

    update templates

--- app-dashboard/src/app/components/menu-level/menu-level.component.ts
+++ app-dashboard/src/app/components/menu-level/menu-level.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-level.component.scss']
 })
 export class MenuLevelComponent {
-
+  subLinks: any[] = [
+    {name: 'Menu Level 1.1', path: 'menu-level11'},
+    {name: 'Menu Level 1.2', path: 'menu-level12'},
+    {name: 'Menu Level 1.3', path: 'menu-level13'}
+  ];
 }
