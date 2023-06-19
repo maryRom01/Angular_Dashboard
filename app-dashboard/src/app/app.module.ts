6d70395cfb43d8fd494c4b28be0c519a5e2d7bed
Maryna Romanets <mromanets@fingercheck.com>
2023-06-19 07:41:20 -0400

    add module - core

--- app-dashboard/src/app/app.module.ts
+++ app-dashboard/src/app/app.module.ts
@@ -1,40 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { HeaderComponent } from './core/header/header.component';
-import { NavigationPanelComponent } from './core/navigation-panel/navigation-panel.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TablesComponent } from './components/tables/tables.component';
 import { ComponentsUiComponent } from './components/components-ui/components-ui.component';
-import { MenuLevel11Component } from './core/navigation-panel/menu-level11/menu-level11.component';
-import { MenuLevel12Component } from './core/navigation-panel/menu-level12/menu-level12.component';
-import { MenuLevel13Component } from './core/navigation-panel/menu-level13/menu-level13.component';
 import { MapsComponent } from './components/maps/maps/maps.component';
-import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    NavigationPanelComponent,
     DashboardComponent,
     TablesComponent,
     ComponentsUiComponent,
-    MenuLevel11Component,
-    MenuLevel12Component,
-    MenuLevel13Component,
     MapsComponent,
-    PageNotFoundComponent,
     MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app-dashboard/src/app/core/core.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationPanelComponent } from './navigation-panel/navigation-panel.component';
+import { MenuLevel11Component } from './navigation-panel/menu-level11/menu-level11.component';
+import { MenuLevel12Component } from './navigation-panel/menu-level12/menu-level12.component';
+import { MenuLevel13Component } from './navigation-panel/menu-level13/menu-level13.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CoreComponent } from './core.component';
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    NavigationPanelComponent,
+    MenuLevel11Component,
+    MenuLevel12Component,
+    MenuLevel13Component,
+    PageNotFoundComponent,
+    CoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [CoreComponent],
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+    NavigationPanelComponent,
+    MenuLevel11Component,
+    MenuLevel12Component,
+    MenuLevel13Component,
+    PageNotFoundComponent,
+    CoreComponent
+  ]
+})
+export class CoreModule { }
